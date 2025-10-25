bf9512ffca8deecfc21c1a51b52a306dcb3927a6
Anton Alekseev <atix.wind@gmail.com>
2025-10-25 14:36:09 +0300

    M4: work in progress-0,5

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.main),
-    path('about/', views.about),
+    path('', views.main, name="home"),
+    path('about/', views.about, name="about"),
     path("item/<int:id>/", views.item_page, name="item_page"),
-    path("items/", views.items_def),
-]
+    path("items/", views.items_def, name="items_list"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- MainApp/views.py
+++ MainApp/views.py
@@ -4,20 +4,12 @@ from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 
 IMAGE_URLS = {
-    1: "https://room78.net/wp-content/uploads/2022/06/gukb4qk....jpg",
+    1: "https://ae04.alicdn.com/kf/Sd265282efa0449798ad7daaa10266e61n.jpg",
     2: "https://img.joomcdn.net/02aaa28e02ea36a98dff0d50629eb6b7abb303a2_original.jpeg",
     3: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoB49TGPkGMQeN5BA4Fmv3NH9ZNVTnGJLsfg&s",
     4: "https://marr.ru/upload/resize_cache/webp/iblock/21b/adaj3t6awf7l5bpts8j9tpaedis6wqkc.webp",
     5: "https://vintageleder.ua/upload/products/img/large/kepka_huliganka_kozhanaya_muzhskaya_gatsby_art_23_vintage_leder.jpg",
 }
-
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5, "img_url":"https://room78.net/wp-content/uploads/2022/06/gukb4qkvixirslxpsa6fcuk9u7qmvmsnpgj3yl1buo2mnrpttbvzatyzvgq5mv923kp-n16bpgtgiwa6enuhsi0k.jpg"},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2, "img_url":"https://img.joomcdn.net/02aaa28e02ea36a98dff0d50629eb6b7abb303a2_original.jpeg"},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12, "img_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoB49TGPkGMQeN5BA4Fmv3NH9ZNVTnGJLsfg&s"},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0, "img_url":"https://marr.ru/upload/resize_cache/webp/iblock/21b/adaj3t6awf7l5bpts8j9tpaedis6wqkc.webp"},
-   {"id": 8, "name": "Кепка" ,"quantity":124, "img_url":"https://vintageleder.ua/upload/products/img/large/kepka_huliganka_kozhanaya_muzhskaya_gatsby_art_23_vintage_leder.jpg"},
-]
 
 def main(request):
     context = {
@@ -44,16 +36,14 @@ def about(request):
 
 def item_page(request, id: int):
 
-    """ TODO: get item by id from db <-- DONE!!"""
-
     item = get_object_or_404(Item, pk=id)
     image_url = IMAGE_URLS.get(item.id)    
+    
     return render(request, "item_page.html", {"item": item, "image_url": image_url})
     
 
 def items_def(request):
-    
-    """ TODO: get all items from db <-- DONE!!"""
-
+  
     items_qs = Item.objects.all().order_by('id')
+    
     return render(request, "items_list.html", {"SKU_List": items_qs})
